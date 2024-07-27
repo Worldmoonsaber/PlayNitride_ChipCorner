a42480984c1b8157ca326f6089bb7c66f4f0cb28
Worldmoonsaber <40863617+Worldmoonsaber@users.noreply.github.com>
2024-07-27 10:00:40 +0800

    已可以檢出 輸出 角點與角度
    待拿到實際影像後 用來調整演算法

--- ChipCorner/main.cpp
+++ ChipCorner/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     Mat img,result, grayimg;
-    img = imread("C:\\Git\\Code\\ChipCorner\\3.bmp");
+    img = imread("C:\\Users\\Chien\\Documents\\GitHub\\PlayNitride_ChipCorner\\3.bmp");
 
     paramChipCorner pm;
        
@@ -53,18 +53,9 @@ int main()
         grayimg = img;
 
     equalizeHist(grayimg, equalImg);
-
     Mat element = getStructuringElement(MORPH_RECT, Size(10, 10));
-
     cv::morphologyEx(equalImg, equalImg, MORPH_OPEN, element, Point(-1, -1));
-
     threshold(equalImg, result, 240, 255, THRESH_BINARY);
-
-
-    //auto t_flag1 = std::chrono::high_resolution_clock::now();
-    //double elapsed_time_ms = std::chrono::duration<double, std::milli>(t_flag1 - t_start).count();
-    //std::cout << "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
-    //std::cout << "flag 1 spend time :: " << elapsed_time_ms << endl;
 
     float maxArea = ra * rb * max(ra_Max,rb_Max);
     float minArea = ra * rb * min(ra_Min,rb_Min);
@@ -72,38 +63,15 @@ int main()
     vector<BlobInfo> vChips_Comfirmed;
     vector<BlobInfo> vChips_NotSure;
 
-    //auto t_flag15 = std::chrono::high_resolution_clock::now();
-    //elapsed_time_ms = std::chrono::duration<double, std::milli>(t_flag15 - t_flag1).count();
-    //std::cout << "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
-    //std::cout << "flag 1.5 spend time :: " << elapsed_time_ms << endl;
-
-    //for (int i = 0; i < vChips.size(); i++)
-    //    _debugArea.push_back(vChips[i].Points());
-
-    //for (int i = 0; i < _debugArea.size(); i++)
-    //    drawContours(debugImg, _debugArea, i, Scalar(255, 255, 255));
-
-    //_debugArea.clear();
-
     float _compactness_ideal=(2+ ra/rb+ rb / ra )/CV_PI;
 
     for (int i = 0; i < vChips.size(); i++)
     {
         if (vChips[i].Rectangularity() > 0.7)
-        {
             vChips_Comfirmed.push_back(vChips[i]);
-        }
         else if (vChips[i].Compactness() < _compactness_ideal * 1.2 && vChips[i].Compactness() > 0.8*_compactness_ideal)
             vChips_Comfirmed.push_back(vChips[i]);// 通常會找到 Chip 有所滑動的情況
-        //
-        //  Rectangularity 與 Compactness 可能會出現條件互斥的情況
-        //
     }
-
-    //auto t_flag2 = std::chrono::high_resolution_clock::now();
-    //elapsed_time_ms = std::chrono::duration<double, std::milli>(t_flag2 - t_flag1).count();
-    //std::cout << "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
-    //std::cout << "flag 2 spend time :: " << elapsed_time_ms << endl;
 
     
     if (vChips_Comfirmed.size() < 3)
@@ -112,57 +80,27 @@ int main()
         throw "CAN NOT FIND ENOUGH Chips On Image";
     }
 
-
-    //equalImg = Mat(img.size(), CV_8UC1);
-
     // 測試
     vector<Point> _allArea;
 
     for (int i = 0; i < vChips_Comfirmed.size(); i++)
-    {
-      //  _debugArea.push_back(vChips_Comfirmed[i].Points());
-
-       // for (int j = 0; j < vChips_Comfirmed[i].contour().size(); j++)
-            
         _allArea.push_back(vChips_Comfirmed[i].Center());
-    }
 
     
     for (int i = 0; i < _debugArea.size(); i++)
-    {
         drawContours(grayimg, _debugArea, i, Scalar(255, 255, 255));
-    }
-
 
     RotatedRect minrect = minAreaRect(_allArea);
 
     for (int i = 0; i < _debugArea.size(); i++)
         drawContours(grayimg, _debugArea, i, Scalar(255, 255, 255));
     
-
     cv::Point2f vertices2f_2[4];
     minrect.points(vertices2f_2);
     cv::Point vertices[4];
 
     for (int i = 0; i < 4; ++i)
         vertices[i] = vertices2f_2[i];
-
-    cv::line(grayimg,
-        vertices[0], vertices[1],
-        Scalar(255, 255, 255));
-
-    cv::line(grayimg,
-        vertices[1], vertices[2],
-        Scalar(255, 255, 255));
-
-    cv::line(grayimg,
-        vertices[2], vertices[3],
-        Scalar(255, 255, 255));
-
-    cv::line(grayimg,
-        vertices[3], vertices[0],
-        Scalar(255, 255, 255));
-    
 
     int nMid = 0;
     float dist = INTMAX_MAX;
@@ -182,12 +120,67 @@ int main()
 
     }
 
+    cv::Point2f fCornerPt = vertices[nMid];
+
+    if (fCornerPt.x > fChip_Pitch &&
+        fCornerPt.x < img.cols - fChip_Pitch &&
+        fCornerPt.y> fChip_Pitch &&
+        fCornerPt.y < img.rows - fChip_Pitch
+        )
+    {
+        //OK 在適當範圍
+    }
+    else
+    {
+        notFoundReason = 2;
+        throw "No Obivious Chip Corner Please Move The Panel";
+    }
+
+    int x_sample_level=-1;
+    dist = INTMAX_MAX;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        if (i == nMid)
+            continue;
+
+        float cur_dist = (vertices[nMid].y - vertices[i].y) * (vertices[nMid].x - vertices[i].y);
+
+        if (cur_dist < dist)
+        {
+            dist = cur_dist;
+            x_sample_level = i;
+        }
+
+    }
 
 
+    cv::line(grayimg,
+        vertices[x_sample_level], vertices[nMid],
+        Scalar(255, 255, 255));
+
+    cv::line(grayimg,
+        vertices[x_sample_level], Point2f(vertices[nMid].x,vertices[x_sample_level].y),
+        Scalar(255, 255, 255));
+
+    float fAngle=0;
+
+    float xx = vertices[nMid].x - vertices[x_sample_level].x;
+    float yy = vertices[nMid].y - vertices[x_sample_level].y;
+
+    float atanVal = atan(yy / xx) * 180.0 / CV_PI;
+
+    if (vertices[nMid].x > vertices[x_sample_level].x)
+        atanVal *= -1;
+
+    float angleOut = atanVal;
 
     auto t_end = std::chrono::high_resolution_clock::now();
     auto elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end - t_start).count();
     std::cout << "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
+    std::cout << "Corner Point :: " << fCornerPt << endl;
+
+    std::cout << "Angle :: " << angleOut << endl;
 
     std::cout << "spend time :: " << elapsed_time_ms << endl;
     std::cout << "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
